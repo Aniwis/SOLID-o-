13a761e98401a2d6e0976200a21c631a42c27a85
Ana <milenaaranceta@hotmail.com>
2021-09-27 22:04:07 -0500

    wip

--- Assets/Scripts/selectionManager.cs
+++ Assets/Scripts/selectionManager.cs
@@ -6,52 +6,45 @@ using UnityEngine;
 public class selectionManager : MonoBehaviour
 {
 
-    [SerializeField] private Material newMaterial;
     [SerializeField] private string selectableTag = "selectable";
     [SerializeField] private Material defaultMaterial;
-    RaycastHit hit;
+    [SerializeField] private Material newMaterial;
+
 
     private Transform _selection;
-   // private States _currentState;
     void Update()
     {
 
         if (_selection != null)
         {
-            OnSpace();
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
         }
+
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
-        
-        if(Physics.Raycast(ray, out hit))
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
         {
-            OnObject();
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = newMaterial;
+                }
+
+                _selection = selection;
+            }
+
         }
 
        
 
     }
-    public void OnObject()
-    {
-        var selection = hit.transform;
-        if (selection.CompareTag(selectableTag))
-        {
-            var selectionRenderer = selection.GetComponent<Renderer>();
-            if (selectionRenderer != null)
-            {
-                selectionRenderer.material = newMaterial;
-            }
-
-            _selection = selection;
-        }
-        
-    }
-
-    public void OnSpace()
-    {
-        var selectionRenderer = _selection.GetComponent<Renderer>();
-        selectionRenderer.material = defaultMaterial;
-        _selection = null;
-    }
+    
 
 
 }
