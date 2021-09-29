2debf8751422e5289676c41f249afcf29f1b9d88
Ana <milenaaranceta@hotmail.com>
2021-09-28 19:25:27 -0500

    Mouse click working

--- Assets/Scripts/selectionManager.cs
+++ Assets/Scripts/selectionManager.cs
@@ -1,6 +1,8 @@
 using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class selectionManager : MonoBehaviour
@@ -8,19 +10,15 @@ public class selectionManager : MonoBehaviour
 
     [SerializeField] private string selectableTag = "selectable";
     [SerializeField] private Material defaultMaterial;
-    [SerializeField] private Material newMaterial;
+    [SerializeField] private Material happyMaterial;
+    [SerializeField] private Material scaredMaterial; 
 
 
     private Transform _selection;
     void Update()
     {
 
-        if (_selection != null)
-        {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material = defaultMaterial;
-            _selection = null;
-        }
+       
 
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
         RaycastHit hit = new RaycastHit();
@@ -30,22 +28,54 @@ public class selectionManager : MonoBehaviour
             var selection = hit.transform;
             if (selection.CompareTag(selectableTag))
             {
-                var selectionRenderer = selection.GetComponent<Renderer>();
-                if (selectionRenderer != null)
-                {
-                    selectionRenderer.material = newMaterial;
-                }
+                if (Input.GetMouseButtonDown(0))
+                    HandleClick(selection, 0);
 
-                _selection = selection;
+                if (Input.GetMouseButtonDown(1))
+                    HandleClick(selection, 1);
+
+                if (Input.GetMouseButtonDown(2))
+                    CleanColor();
+
+
             }
+
+            
+
 
         }
 
-       
+    }
+
+    void HandleClick(Transform selection, int type)  
+    {
+        var selectionRenderer = selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            
+            if (type == 0)
+            {
+                selectionRenderer.material = happyMaterial;
+            }
+            if (type == 1)
+            {
+                selectionRenderer.material = scaredMaterial;
+            }
+        }
+
+        _selection = selection;
 
     }
-    
 
-
+    void CleanColor()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+    }
+   
 }
 
